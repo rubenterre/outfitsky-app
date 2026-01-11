3510cccce78debbfbf1c4a0b3ce46baa6b787ffe
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2026-01-11 21:00:12 +0100

    Update: favicon style design

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DeXAxGhK.js","_app/immutable/chunks/Bx8SL5k3.js","_app/immutable/chunks/BQx_mDMY.js","_app/immutable/chunks/Dq5xo3FP.js","_app/immutable/chunks/C5nbIDZX.js","_app/immutable/chunks/CYnpJbWx.js","_app/immutable/chunks/BYtmbN4C.js"];
+export const imports = ["_app/immutable/nodes/1.ByV_oVga.js","_app/immutable/chunks/Bx8SL5k3.js","_app/immutable/chunks/BQx_mDMY.js","_app/immutable/chunks/Dq5xo3FP.js","_app/immutable/chunks/C5nbIDZX.js","_app/immutable/chunks/e1PPC8RS.js","_app/immutable/chunks/BYtmbN4C.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .svelte-kit/output/server/nodes/0.js
+++ .svelte-kit/output/server/nodes/0.js
@@ -3,6 +3,6 @@
 export const index = 0;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.oH4k-4Ki.js","_app/immutable/chunks/Bx8SL5k3.js","_app/immutable/chunks/BQx_mDMY.js","_app/immutable/chunks/C5nbIDZX.js","_app/immutable/chunks/Bm5pYm2A.js","_app/immutable/chunks/Dp_DQ4Z4.js","_app/immutable/chunks/C7YlgDcK.js","_app/immutable/chunks/BYtmbN4C.js","_app/immutable/chunks/CvEjrGun.js","_app/immutable/chunks/DDPc4jWK.js","_app/immutable/chunks/CYnpJbWx.js"];
+export const imports = ["_app/immutable/nodes/0.D0NUqdJM.js","_app/immutable/chunks/Bx8SL5k3.js","_app/immutable/chunks/BQx_mDMY.js","_app/immutable/chunks/C5nbIDZX.js","_app/immutable/chunks/Bm5pYm2A.js","_app/immutable/chunks/Dp_DQ4Z4.js","_app/immutable/chunks/C7YlgDcK.js","_app/immutable/chunks/BYtmbN4C.js","_app/immutable/chunks/CvEjrGun.js","_app/immutable/chunks/IdXLi3lb.js","_app/immutable/chunks/e1PPC8RS.js"];
 export const stylesheets = ["_app/immutable/assets/0.DlkGDMMe.css"];
 export const fonts = [];
